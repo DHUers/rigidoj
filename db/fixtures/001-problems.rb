8a218dcdc1e9c3feb7130d99458d2ee6a3b52e51
Erick Guan <fantasticfears>
2014-11-27 09:49:34 +0800

    problem fixture

---
+++ db/fixtures/001-problems.rb
@@ -0,0 +1,21 @@
+Problem.seed do |p|
+  p.author_id = -1
+  p.title = 'A + B Problem'
+  p.memory_limit = 65535
+  p.time_limit = 1000
+  p.author_id = -1
+  p.raw = <<RAW
+## Description
+Give you two integers A and B, please output A+B.
+
+## Sample Input
+```
+1 2
+```
+
+## Sample Output
+```
+3
+```
+RAW
+end
