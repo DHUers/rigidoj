a8e508059857e827ffecaa5664c64f3a8479658e
Erick Guan <fantasticfears@gmail.com>
2014-11-28 11:30:39 +0800

    more complicated json object for problems

---
+++ db/migrate/20141128032324_change_problem_limit_type.rb
@@ -0,0 +1,15 @@
+class ChangeProblemLimitType < ActiveRecord::Migration
+  def up
+    remove_column :problems, :memory_limit
+    remove_column :problems, :time_limit
+    add_column :problems, :memory_limit, :json, default: '{"default":65535}'
+    add_column :problems, :time_limit, :json, default: '{"default":1000}'
+  end
+
+  def down
+    remove_column :problems, :memory_limit
+    remove_column :problems, :time_limit
+    add_column :problems, :memory_limit, :integer, default: 65535
+    add_column :problems, :time_limit, :integer, default: 1000
+  end
+end
