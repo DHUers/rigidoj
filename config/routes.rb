4aa60b38283e2d283e1f8590980e2c6d4aa42393
Erick Guan <fantasticfears@gmail.com>
2014-12-06 02:04:28 +0800

    admin namespace

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,15 @@ Rails.application.routes.draw do
     resources :solutions
   end
 
+  namespace :admin do
+    root to: 'admin#dashboard'
+  end
+
   get '/signup', to: 'users#new', as: :registration
   get '/login', to: 'sessions#new', as: :login
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy', as: :logout
   get '/about', to: 'statics#about', as: :about
   get '/help', to: 'statics#help', as: :help
-  root 'statics#home'
+  root to: 'statics#home'
 end
