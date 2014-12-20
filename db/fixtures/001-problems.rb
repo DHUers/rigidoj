7a079d2d9329ec9944f48e2e87cda0c7581d26b0
Erick Guan <fantasticfears@gmail.com>
2014-12-21 00:00:04 +0800

    herodocs for multi-line

--- db/fixtures/001-problems.rb
+++ db/fixtures/001-problems.rb
@@ -4,18 +4,20 @@ Problem.seed do |p|
   p.public = true
   p.draft = false
   p.judge_type = :full_text
-  p.raw = <<RAW
-## Description
-Give you two integers A and B, please output A+B.
-
-## Sample Input
-```
-1 2
-```
-
-## Sample Output
-```
-3
-```
-RAW
+  p.raw = <<-END.gsub(/^\s+\|/, '')
+    |## Description
+    |Give you two integers A and B, please output A+B.
+    |
+    |## Sample Input
+    |```
+    |1 2
+    |```
+    |
+    |## Sample Output
+    |```
+    |3
+    |```
+    |RAW
+    |end
+  END
 end
--- db/fixtures/002-posts.rb
+++ db/fixtures/002-posts.rb
@@ -1,9 +1,9 @@
 Post.seed do |p|
   p.id = 1
   p.title = 'Welcome to RigidOJ!'
-  p.raw = <<END
-Add or import problem to get started.
-END
+  p.raw = <<-END.gsub(/^\s+\|/, '')
+    |Add or import problem to get started.
+  END
   p.user_id = -1
   p.published = true
 end
@@ -11,11 +11,11 @@ end
 Post.seed do |p|
   p.id = 2
   p.title = 'Help'
-  p.raw = <<END
-Helpful information for your user.
-
-Edit this post to get started.
-END
+  p.raw = <<-END.gsub(/^\s+\|/, '')
+    |Helpful information for your user.
+    |
+    |Edit this post to get started.
+  END
   p.user_id = -1
   p.published = false
 end
@@ -23,11 +23,11 @@ end
 Post.seed do |p|
   p.id = 3
   p.title = 'About'
-  p.raw = <<END
-About this site & TOS information.
-
-Edit this post to get started.
-END
+  p.raw = <<-END.gsub(/^\s+\|/, '')
+    |About this site & TOS information.
+    |
+    |Edit this post to get started.
+  END
   p.user_id = -1
   p.published = false
 end
