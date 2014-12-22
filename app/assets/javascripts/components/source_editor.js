745a0e6834ca871ed25e43be0add09dcd7a8508d
Erick Guan <fantasticfears@gmail.com>
2014-12-22 23:18:59 +0800

    fix source editor

--- app/assets/javascripts/components/source_editor.js
+++ app/assets/javascripts/components/source_editor.js
@@ -1,50 +1,60 @@
 $(document).on('page:change', function() {
   // setup editor
-  var editorContainer = $('.source-editor');
-  if (!editorContainer.length)
-    return;
+  $('.source-editor').each(function() {
+    var container = $(this);
+    var mode = container.data('mode');
+    var editorArea = container.find('textarea');
+    var previewArea = container.find('.preview-area');
 
-  var mode = editorContainer.data('mode');
-  var editorArea = editorContainer.find('.source-editor-textarea');
-  var previewArea = editorContainer.find('.preview-area');
-  var actions = editorContainer.find('.actions select');
-  var editor = ace.edit(editorArea.attr('id'));
+    var actions = container.find('.actions select');
 
-  editor.setTheme('ace/theme/yesterday');
-  editor.getSession().setMode('ace/mode/' + mode);
-  actions.each(function() {
-    var select = $(this);
-    var optionName = select.data('option');
-    editor.setOption(optionName, select.val());
-    select.on('change', function() {
-      editor.setOption(optionName, 0);
-    });
-  });
+    // build a edit div for ACE since ACE can't load in a textarea
+    var editDiv = $('<div>', {
+      position: 'absolute',
+      width: editorArea.width(),
+      height: editorArea.height(),
+      'class': editorArea.attr('class')
+    }).insertBefore(editorArea);
+    editorArea.css('display', 'none');
 
-  // monitor editor tab click events
-  editorContainer.on('click', 'a', function() {
-    $('.edit-tab').each(function() {
-      $(this).removeClass('selected');
-    });
+    var editor = ace.edit(editDiv[0]);
 
-    var tab = $(this);
-    tab.addClass('selected');
-    var previewUrl = tab.data('preview-url');
-    if (previewUrl) {
-      editorArea.addClass('hidden');
-      previewArea.removeClass('hidden');
-      $.ajax({
-        url: previewUrl,
-        type: 'POST',
-        data: {
-          raw: editor.getSession().getValue()
-        }
-      }).done(function(html) {
-        $('.preview-area').empty().append(html);
+    editor.setTheme('ace/theme/yesterday');
+    editor.getSession().setMode('ace/mode/' + mode);
+    actions.each(function() {
+      var select = $(this);
+      var optionName = select.data('option');
+      editor.setOption(optionName, select.val());
+      select.on('change', function() {
+        editor.setOption(optionName, 0);
       });
-    } else {
-      editorArea.removeClass('hidden');
-      previewArea.addClass('hidden');
-    }
+    });
+
+    // monitor editor tab click events
+    container.on('click', 'a', function() {
+      $('.edit-tab').each(function () {
+        $(this).removeClass('selected');
+      });
+
+      var tab = $(this);
+      tab.addClass('selected');
+      var previewUrl = tab.data('preview-url');
+      if (previewUrl) {
+        editDiv.addClass('hidden');
+        previewArea.removeClass('hidden');
+        $.ajax({
+          url: previewUrl,
+          type: 'POST',
+          data: {
+            raw: editor.getSession().getValue()
+          }
+        }).done(function (html) {
+          $('.preview-area').empty().append(html);
+        });
+      } else {
+        editDiv.removeClass('hidden');
+        previewArea.addClass('hidden');
+      }
+    });
   });
 });
