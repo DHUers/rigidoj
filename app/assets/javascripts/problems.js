409560d7e3160810e1fd4f241e8c1f91356402da
Erick Guan <fantasticfears@gmail.com>
2014-12-23 01:09:48 +0800

    problem judge type options

--- app/assets/javascripts/problems.js
+++ app/assets/javascripts/problems.js
@@ -1,9 +1,19 @@
 $(document).on('page:change', function() {
 
+  initalValue = $('#problem_judge_type').val();
+
+  $('.judge-source-group').each(function(_, v) {
+    if ($(v).data('type') === initalValue) {
+      $(this).removeClass('hidden');
+    } else {
+      $(this).addClass('hidden');
+    }
+  });
+
   $('#problem_judge_type').on('change', function() {
     var selectedValue = $(this).val();
-    $('.judge-source-group').each(function(i) {
-      if (i == selectedValue) {
+    $('.judge-source-group').each(function(_, v) {
+      if ($(v).data('type') === selectedValue) {
         $(this).removeClass('hidden');
       } else {
         $(this).addClass('hidden');
