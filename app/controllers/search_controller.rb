40ab89415b830332853620801aa02f282585ff1b
Erick Guan <fantasticfears@gmail.com>
2015-01-05 09:46:30 +0800

    remove serialized [skip ci]

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,10 @@
+require_dependency 'search'
+
+class SearchController < ApplicationController
+  def query
+    params.require :term
+
+    search = Search.new(params[:term], search_args.symbolize_keys)
+    result = search.execute
+  end
+end
