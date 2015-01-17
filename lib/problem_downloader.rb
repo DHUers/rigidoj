5279fcda96f1c4fe9e000ed12893f9ce5a99f9d2
Erick Guan <fantasticfears@gmail.com>
2015-01-17 17:53:04 +0800

    create problem according strategy

--- lib/problem_downloader.rb
+++ lib/problem_downloader.rb
@@ -12,18 +12,24 @@ class ProblemDownloader
     set_downloader_strategy
   end
 
-  def set_downloader_strategy
-    case @oj_name
-    when :hdu then @downloader = HDUStrategy.new
-    when :poj then @downloader = POJStrategy.new
-    when :uva then @downloader = UVAStrategy.new
-    when :uva_live then @downloader = UVALiveStrategy.new
-    when :zoj then @downloader = ZOJStrategy.new
-    else raise NoDownloadStrategy
-    end
+  def download_and_create_problem
+    Problem.new(download)
   end
+
+  private
 
   def download
     @downloader.download(self)
+  end
+
+  def set_downloader_strategy
+    case @oj_name
+      when :hdu then @downloader = HDUStrategy.new
+      when :poj then @downloader = POJStrategy.new
+      when :uva then @downloader = UVAStrategy.new
+      when :uva_live then @downloader = UVALiveStrategy.new
+      when :zoj then @downloader = ZOJStrategy.new
+      else raise NoDownloadStrategy
+    end
   end
 end
