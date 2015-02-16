0e56cb11fdc8b982b1271db722d556ad25db210b
wujysh <wujysh@gmail.com>
2015-02-16 12:12:50 +0800

    support Codeforces

--- lib/problem_downloader/codeforces_strategy.rb
+++ lib/problem_downloader/codeforces_strategy.rb
@@ -7,29 +7,42 @@ class ProblemDownloader::CodeforcesStrategy
       title: @title,
       raw: @raw_content,
       remote_proxy_vendor: "Codeforces,#{@id}",
-      default_time_limit: @time_limit[2].to_i * 1000,
-      default_memory_limit: @memory_limit[2]
+      default_time_limit: @time_limit[1].to_i * 1000,
+      default_memory_limit: @memory_limit[1].to_i * 1024
     }
     params
   end
 
   def download(context)
-    contest_id = context.problem_index
-    problem_id = context.problem_index
+    contest_id = context.problem_index.gsub(/\D.*/, '')
+    problem_id = context.problem_index.gsub(/^\d*/, '')
     url = "http://codeforces.com/problemset/problem/#{contest_id}/#{problem_id}"
     html = Nokogiri::HTML(open(url))
     raw = html.inner_html.encode('utf-8')
 
-    return false if raw.include?('no such problem')
+    return false if raw.include?('No such problem') || raw.include?('No such contest')
 
     @id = context.problem_index
     @raw_content = ''
 
-    # TODO
+    html.css('.header').remove
+    content = html.css('.problem-statement').inner_html.encode('utf-8')
+                  .gsub('</p>', "\n\n").gsub('<br>', "\n").gsub('<li>', "\n*")
+                  .gsub('<div class="section-title">', '## ')
+                  .gsub('<div class="title">', '### ')
 
-    @title = /#{@id}[.] ([\s\S]*?)</.match(raw)[1].strip
-    @time_limit = /Time limit( per test)?: ([\d\.]*)/i.match(raw)
-    @memory_limit = /Memory limit( per test)?: ([\d]*)/i.match(raw)
+    image = ''
+    html.search('img.tex-graphics').each do |img|
+      img_html = img.to_html
+      image << img_html << "\n\n"
+    end
+
+    fragment = Nokogiri::HTML.fragment(content)
+    @raw_content << "## Description\n" << fragment.text.strip.gsub(/\s+$/, "\n") << "\n\n" << image
+
+    @title = /<div class="title">\s*#{problem_id}. ([\s\S]*?)<\/div>/.match(raw)[1].strip
+    @time_limit = /<\/div>([\d\.]+) seconds?\s*<\/div>/i.match(raw)
+    @memory_limit = /<\/div>(\d+) megabytes\s*<\/div>/i.match(raw)
     @raw_content << "## Remote Url\n" << "[Codeforces #{@id}: #{@title}](#{url})\n"
     html
   end
