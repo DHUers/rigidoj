d143c7fe00273c3d329f483dcffd820c0d481c80
Erick Guan <fantasticfears@gmail.com>
2015-02-28 11:21:53 +0800

    Revert "solution time should be ceiled to the minute"
    This reverts commit 1f73f32cc74513db74ef0b29311504d7f757a9c0.

--- spec/components/contest_ranking_spec.rb
+++ spec/components/contest_ranking_spec.rb
@@ -22,15 +22,6 @@ describe ContestRanking do
       end
     end
 
-    describe 'solution time' do
-      it 'should be ceiled to the minute' do
-        @time1 = Time.zone.local(2099, 1, 1, 14, 0, 0)  # 2099-01-01 14:00:00
-        @time2 = Time.zone.local(2099, 1, 1, 13, 59, 1) # 2099-01-01 13:59:01
-        expect(@contest.duration_with_started_at_in_minute(@time1)).to eq 360
-        expect(@contest.duration_with_started_at_in_minute(@time2)).to eq 360
-      end
-    end
-
     describe 'solutions ranking' do
       # [1]: 4h = 240min
       # [2]: 5h = 300min
@@ -169,15 +160,6 @@ describe ContestRanking do
     after :all do
       User.delete_all
       Contest.delete_all
-    end
-
-    describe 'solution time' do
-      it 'should be ceiled to the minute' do
-        @time1 = Time.zone.local(2099, 1, 1, 14, 0, 0)  # 2099-01-01 14:00:00
-        @time2 = Time.zone.local(2099, 1, 1, 13, 59, 1) # 2099-01-01 13:59:01
-        expect(@contest.duration_with_started_at_in_minute(@time1)).to eq 360
-        expect(@contest.duration_with_started_at_in_minute(@time2)).to eq 360
-      end
     end
 
     describe '.frozen?' do
