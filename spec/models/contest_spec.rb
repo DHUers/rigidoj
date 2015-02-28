c95aa1f023353684b8a524b2cc6b60aa4bc06e1c
Erick Guan <fantasticfears@gmail.com>
2015-02-28 20:16:05 +0800

    another spot timezone

--- spec/models/contest_spec.rb
+++ spec/models/contest_spec.rb
@@ -7,8 +7,8 @@ describe Contest do
     end
 
     it 'should be ceiled to the minute' do
-      @time1 = Time.zone.local(2099, 1, 1, 14, 0, 0)  # 2099-01-01 14:00:00
-      @time2 = Time.zone.local(2099, 1, 1, 13, 59, 1) # 2099-01-01 13:59:01
+      @time1 = Time.zone.local(2099, 1, 1, 6, 0, 0)  # 2099-01-01 06:00:00
+      @time2 = Time.zone.local(2099, 1, 1, 5, 59, 1) # 2099-01-01 05:59:01
       expect(@contest.duration_with_started_at_in_minute(@time1)).to eq 360
       expect(@contest.duration_with_started_at_in_minute(@time2)).to eq 360
     end
