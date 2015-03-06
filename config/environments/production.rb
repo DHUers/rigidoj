5ed8965bd0a442717dab540d778641dbb711fa30
Erick Guan <fantasticfears@gmail.com>
2015-03-07 00:39:55 +0800

    only applied prefix to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.prefix = '/new/assets'
 end
