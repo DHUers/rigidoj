e9d35af09604812be79f9716920ecc3a2c9e1201
Erick Guan <fantasticfears@gmail.com>
2015-03-07 00:43:15 +0800

    tell controll the world is in the well

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_controller.relative_url_root = '/new'
   config.assets.prefix = '/new/assets'
 end
