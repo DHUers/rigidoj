91afe9f974875339e265414cad20c90f58946e64
Erick Guan <fantasticfears@gmail.com>
2015-03-09 01:43:22 +0800

    add RIGIDOJ_REFILE_TOKEN for uploads security

--- config/initializers/refile_config.rb
+++ config/initializers/refile_config.rb
@@ -1,4 +1,5 @@
 Refile.types[:code] = Refile::Type.new(:code,
                                        content_type: %w[text/plain application/octet-stream]
 )
-Refile.secret_key = SecureRandom.hex(64).to_s
+puts GlobalSetting.refile_token
+Refile.secret_key = GlobalSetting.refile_token || SecureRandom.hex(64).to_s
