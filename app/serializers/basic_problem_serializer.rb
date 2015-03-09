c4e57f09269bd1bbb908a1f26a8b4d8332748f08
Erick Guan <fantasticfears@gmail.com>
2015-03-09 23:30:05 +0800

    change the serializer

--- app/serializers/basic_problem_serializer.rb
+++ app/serializers/basic_problem_serializer.rb
@@ -4,16 +4,16 @@ class BasicProblemSerializer < ActiveModel::Serializer
   def judge_data
     problem_judge_type = object.judge_type.to_sym
     data = if problem_judge_type == :remote_proxy
-             {vendor: object.remote_proxy_vendor}
+             { vendor: object.remote_proxy_vendor }
            else
-             {input_file_url: object.input_file_url,
-              output_file_url: object.output_file_url,
-              judge_limits: object.judge_limits,
-              per_case_limit: object.per_case_limit}
+             { input_file: "/store/#{object.input_file_id}/input_file",
+               output_file: "/store/#{object.output_file_id}/output_file",
+               judge_limits: object.judge_limits,
+               per_case_limit: object.per_case_limit }
            end
     if (problem_judge_type == :program_comparison)
-      data.merge!({judger_program_source: object.judger_program.read,
-                   judger_program_platform: object.judger_program_platform})
+      data.merge!({ judger_program_source: object.judger_program.read,
+                    judger_program_platform: object.judger_program_platform })
     end
     data
   end
