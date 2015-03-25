59b629c5742e0c05d281bcc0856321b298966cee
Erick Guan <fantasticfears@gmail.com>
2015-03-25 11:08:20 +0800

    fix groups

--- app/policies/group_policy.rb
+++ app/policies/group_policy.rb
@@ -1,14 +1,24 @@
 class GroupPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      if user && user.staff?
+        scope.all
+      else
+        scope.where(visible: true)
+      end
+    end
+  end
+
   def create?
     admin?
   end
 
   def admin?
-    user.admin?
+    user && user.admin?
   end
 
   def show?
-    user
+    record.visible || user && user.staff?
   end
 
   def update?
