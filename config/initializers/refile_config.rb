b895e0065fdca248150b33c0a161354bbb93a05d
Erick Guan <fantasticfears@gmail.com>
2015-07-01 08:05:13 +0800

    this is merged into the rails secret_key_base

--- config/initializers/refile_config.rb
+++ config/initializers/refile_config.rb
@@ -1,6 +1,5 @@
 Refile.types[:code] = Refile::Type.new(:code,
                                        content_type: %w[text/plain]
 )
-Refile.secret_key = GlobalSetting.refile_token || SecureRandom.hex(64).to_s
 
 Refile.store = Refile::Backend::FileSystem.new(Rails.root.join("public/uploads").to_s)
